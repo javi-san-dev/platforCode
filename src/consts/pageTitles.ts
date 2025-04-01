6720f92743ae8069d9cc3b8476569e1bbafe0b56
Javier Sanchez <jsanchez@veridas.com>
2025-04-01 13:57:23 +0200

    refactor components

---
+++ src/consts/pageTitles.ts
@@ -0,0 +1,5 @@
+export const fixedTitle: string = 'XXX - Algorithms platform for macOS, Windows and Linux'
+
+export const pricing: string = `Pricing - XXX`
+
+export const URL: string = "https://challenges-landing.vercel.app"
