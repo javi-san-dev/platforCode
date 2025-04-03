9777939039111a6d358973acbdbba05ce33cc934
Javier Sanchez <jsanchez@veridas.com>
2025-04-03 17:02:11 +0200

    update product name

--- src/consts/pageTitles.ts
+++ src/consts/pageTitles.ts
@@ -1,5 +1,5 @@
-export const fixedTitle: string = 'XXX - Algorithms platform for macOS, Windows and Linux'
+export const fixedTitle: string = 'PlatforCode - Algorithms platform for macOS, Windows and Linux'
 
-export const pricing: string = `Pricing - XXX`
+export const pricing: string = `Pricing - PlatforCode`
 
 export const URL: string = "https://challenges-landing.vercel.app"
